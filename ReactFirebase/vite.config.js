4d1ff9ba7ca000521dde9bdb9cb034e6924a6380
mshalom27 <menshalom07@gmail.com>
2025-04-03 20:06:02 +0530

    Fixed build output

--- ReactFirebase/vite.config.js
+++ ReactFirebase/vite.config.js
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],base: '/Authentication/'
+  plugins: [react(),tailwindcss()],base: '/Authentication/',
+  build: {
+    outDir: '../dist'  // Moves the build output to the correct location
+  }
 })
 
 
